2977facddccd77f45df6908fb6b3ccf92294c32b
Patrick Lee Walker <pwalker@sandiego.edu>
2021-07-18 23:43:41 -0700

    Reformatting changes
    Reformat all scripts to follow PEP8 guidelines - fixed spacings throughout

--- Creating and Manipulating Objects/randomCubes.py
+++ Creating and Manipulating Objects/randomCubes.py
@@ -3,7 +3,7 @@
 import maya.cmds as cmds
 import random
 
-random.seed( 1234 )
+random.seed(1234)
 
 '''
 cubeList = cmds.ls( 'myCube*' )
@@ -11,41 +11,41 @@ if len( cubeList ) > 0:
     cmds.delete( cubeList )
 '''
 
-result = cmds.polyCube( w=1, h=1, d=1, name='myCube#' )
+result = cmds.polyCube(w=1, h=1, d=1, name='myCube#')
 
 #print 'result: ' + str( result )
 
 transformName = result[0]
 
-instanceGroupName = cmds.group( empty=True, name=transformName + '_instance_grp#' )
+instanceGroupName = cmds.group(empty=True, name=transformName + '_instance_grp#')
 
-for i in range( 0, 50 ):
+for i in range(0, 50):
 
-    instanceResult = cmds.instance( transformName, name=transformName + '_instance#' )
+    instanceResult = cmds.instance(transformName, name=transformName + '_instance#')
     
-    cmds.parent( instanceResult, instanceGroupName )
+    cmds.parent(instanceResult, instanceGroupName)
     
-    print('instanceResult: ' + str( instanceResult ))
+    print('instanceResult: ' + str(instanceResult))
     
-    x = random.uniform( -10, 10 )
-    y = random.uniform( 0, 20 )
-    z = random.uniform( -10, 10 )
+    x = random.uniform(-10, 10)
+    y = random.uniform(0, 20)
+    z = random.uniform(-10, 10)
     
-    cmds.move( x, y, z, instanceResult )
+    cmds.move(x, y, z, instanceResult)
     
-    xRot = random.uniform( 0, 360 )
-    yRot = random.uniform( 0, 360 )
-    zRot = random.uniform( 0, 360 )
+    xRot = random.uniform(0, 360)
+    yRot = random.uniform(0, 360)
+    zRot = random.uniform(0, 360)
     
-    cmds.rotate( xRot, yRot, zRot, instanceResult )
+    cmds.rotate(xRot, yRot, zRot, instanceResult)
     
-    scalingFactor = random.uniform( 0.3, 1.5 )
+    scalingFactor = random.uniform(0.3, 1.5)
     
-    cmds.scale( scalingFactor, scalingFactor, scalingFactor, instanceResult )
+    cmds.scale(scalingFactor, scalingFactor, scalingFactor, instanceResult)
 
-cmds.hide( transformName )
+cmds.hide(transformName)
 
-cmds.xform( instanceGroupName, centerPivots=True )
+cmds.xform(instanceGroupName, centerPivots=True)
 
 
 
--- Working with Selections and Constraints/randomInstances.py
+++ Working with Selections and Constraints/randomInstances.py
@@ -3,40 +3,37 @@
 import maya.cmds as cmds
 import random
 
-random.seed( 1234 )
+random.seed(1234)
 
-result = cmds.ls( orderedSelection=True )
+result = cmds.ls(orderedSelection=True)
 
-print('result: %s' % ( result ))
+print('result: %s' % (result))
 
 transformName = result[0]
 
-instanceGroupName = cmds.group( empty=True, name=transformName + '_instance_grp#' )
+instanceGroupName = cmds.group(empty=True, name=transformName + '_instance_grp#')
 
-for i in range( 0, 50 ):
+for i in range(0, 50):
+    instanceResult = cmds.instance(transformName, name=transformName + '_instance#')
 
-    instanceResult = cmds.instance( transformName, name=transformName + '_instance#' )
-    
-    cmds.parent( instanceResult, instanceGroupName )
-    
-    #print 'instanceResult: ' + str( instanceResult )
-    
-    x = random.uniform( -10, 10 )
-    y = random.uniform( 0, 20 )
-    z = random.uniform( -10, 10 )
-    
-    cmds.move( x, y, z, instanceResult )
-    
-    xRot = random.uniform( 0, 360 )
-    yRot = random.uniform( 0, 360 )
-    zRot = random.uniform( 0, 360 )
-    
-    cmds.rotate( xRot, yRot, zRot, instanceResult )
-    
-    scalingFactor = random.uniform( 0.3, 1.5 )
-    
-    cmds.scale( scalingFactor, scalingFactor, scalingFactor, instanceResult )
+    cmds.parent(instanceResult, instanceGroupName)
 
-cmds.hide( transformName )
+    x = random.uniform(-10, 10)
+    y = random.uniform(0, 20)
+    z = random.uniform(-10, 10)
 
-cmds.xform( instanceGroupName, centerPivots=True )
+    cmds.move(x, y, z, instanceResult)
+
+    xRot = random.uniform(0, 360)
+    yRot = random.uniform(0, 360)
+    zRot = random.uniform(0, 360)
+
+    cmds.rotate(xRot, yRot, zRot, instanceResult)
+
+    scalingFactor = random.uniform(0.3, 1.5)
+
+    cmds.scale(scalingFactor, scalingFactor, scalingFactor, instanceResult)
+
+cmds.hide(transformName)
+
+cmds.xform(instanceGroupName, centerPivots=True)
